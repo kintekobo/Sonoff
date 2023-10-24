be1d60195c4c3d708d686fd8e49e71c76ce56b23
kintekobo <chris.mower@gmail.com>
2023-10-24 14:03:27 +0100

    Add files via upload

---
+++ Arduino/SonoffUdpLight/SonoffUdpLight.ino
@@ -0,0 +1,87 @@
+/*
+  Arduino code to control the SONOFF module via UDP
+*/
+
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+#define SSID "Acheron"
+#define PSK "TheFall0fTheH0use0fUsher"
+#define SERIAL
+
+unsigned int localPort = 8888;  // local port to listen on
+unsigned int LED = 13; // Led on board
+unsigned int RELAY = 12; // Power relay
+unsigned int RELAY_ON = HIGH; // Energise the relay
+unsigned int RELAY_OFF = LOW; // De-energise the relay
+unsigned int LED_OFF = HIGH; // Turn the LED on
+unsigned int LED_ON = LOW; // Turn the LED off
+
+// Set this unique to each Sonoff module
+unsigned int MODULE = 1; // Each Sonoff has a different module number
+
+
+// buffers for receiving and sending data
+char packetBuffer[ UDP_TX_PACKET_MAX_SIZE + 1 ];  // buffer to hold incoming packet,
+char replyBuffer[] = "ack\r\n";        // Notify sender that the packet was received OK
+
+WiFiUDP Udp;
+
+void setup() 
+{
+  pinMode( 13, OUTPUT ); 
+  pinMode( 12, OUTPUT ); 
+  digitalWrite( LED, LED_ON ); // Turn the status LED on while initialising
+  digitalWrite( RELAY, RELAY_OFF ); // Make sure that tho relay is off at the start
+  WiFi.mode( WIFI_STA );
+#ifdef SERIAL
+    Serial.begin( 115200 ); 
+    delay( 5000 );
+    Serial.println( "Started.." );
+#endif
+  WiFi.begin( SSID, PSK );
+  while ( WiFi.status() != WL_CONNECTED ) 
+  {
+    delay( 500 );
+  }
+
+#ifdef SERIAL
+    Serial.println( "WiFi Connected" );
+#endif
+
+  Udp.begin( localPort );
+  digitalWrite( LED, LED_OFF ); // Now turn the LED off. No point wasting power
+}
+
+void loop() 
+{
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  if ( packetSize ) 
+  {
+    // read the packet into packetBufffer
+    int n = Udp.read( packetBuffer, UDP_TX_PACKET_MAX_SIZE );
+    packetBuffer[ n ] = 0; // Terminate buffer
+#ifdef SERIAL
+    Serial.println(  packetBuffer  ); // Let's see what was sent  
+#endif
+    if (  packetBuffer[ 0 ] == '*' ) // Valid message with states
+    {
+      if (  packetBuffer[  MODULE  ] == '1' ) // Indicates turn on
+      {
+          digitalWrite( LED, LED_ON ); 
+          digitalWrite( RELAY, RELAY_ON );  
+      }
+      if (  packetBuffer[  MODULE  ] == '0' ) // Indicates turn off
+      {
+          digitalWrite( LED, LED_OFF );
+          digitalWrite( RELAY, RELAY_OFF ); 
+      }
+
+    // Not strictly needed by the other end but it doesn't hurt
+      Udp.beginPacket( Udp.remoteIP(), Udp.remotePort() );
+      Udp.write( replyBuffer );
+      Udp.endPacket();
+    }
+  }
+}
---
+++ Arduino/SonoffUdpLightController/SonoffUdpLightController.ino
@@ -0,0 +1,117 @@
+/*
+  Arduino code to control the SONOFF module via UDP
+*/
+
+#include "WiFi.h"
+#include <WiFiUdp.h>
+
+
+#define SSID "Acheron"
+#define PSK "TheFall0fTheH0use0fUsher"
+#undef SERIALON
+
+unsigned int localPort = 8899;  // local port to listen on
+
+// 
+unsigned int Zone1On = 1; 
+unsigned int Zone2On = 2; 
+unsigned int Zone3On = 3; 
+unsigned int Zone1Off = 4; 
+unsigned int Zone2Off = 5; 
+unsigned int Zone3Off = 6; 
+
+unsigned int PRESSED = LOW; // Ground pin when button pressed
+unsigned int Changed = 1; // If one of the buttons has been pressed
+char packetBuffer[] = "*0000000000";  // buffer to hold outgoing packet, we currently only use 4 chars but a few extra won't hurt
+IPAddress Broadcast( 192,168,0,255 );  // UDP Broadcast address
+WiFiUDP Udp; // Create the UDP object
+
+void setup() 
+{
+// Set the GPIO pins as inputs with pullup so the buttons ground the input
+pinMode( 1, INPUT_PULLUP ); 
+pinMode( 2, INPUT_PULLUP ); 
+pinMode( 3, INPUT_PULLUP ); 
+pinMode( 4, INPUT_PULLUP ); 
+pinMode( 5, INPUT_PULLUP ); 
+pinMode( 6, INPUT_PULLUP ); 
+
+#ifdef SERIALON
+    Serial.begin( 115200 ); 
+    delay( 1000 );
+    Serial.println( "Started.." );
+#endif
+
+  // Start a WiFi connection
+  WiFi.begin( SSID, PSK );
+  while ( WiFi.status() != WL_CONNECTED ) 
+  {
+    delay( 100 );
+  }
+
+#ifdef SERIALON
+    Serial.println( "WiFi Connected" );
+#endif
+
+  // Start a UDP session
+  Udp.begin( localPort );
+}
+
+// Now we loop looking at the six input GPIO pins for a pressed button
+// If found we set the appropriate character in the buffer and set the Changed
+// Flag to indicate that the buffer should be sent
+void loop() 
+{
+
+  if ( digitalRead( Zone1On ) == PRESSED )
+  {
+    packetBuffer[ 1 ] = '1';
+    Changed = 1;
+  }
+
+  if ( digitalRead( Zone2On ) == PRESSED )
+  {
+    packetBuffer[ 2 ] = '1';
+    Changed = 1;
+  }
+
+  if ( digitalRead( Zone3On ) == PRESSED )
+  {
+    packetBuffer[ 3 ] = '1';
+    Changed = 1;
+  }
+
+  if ( digitalRead( Zone1Off ) == PRESSED )
+  {
+    packetBuffer[ 1 ] = '0';
+    Changed = 1;
+  }
+  
+  if ( digitalRead( Zone2Off ) == PRESSED )
+  {
+    packetBuffer[ 2 ] = '0';
+    Changed = 1;
+  }
+
+  if ( digitalRead( Zone3Off ) == PRESSED )
+  {
+    packetBuffer[ 3 ] = '0';
+    Changed = 1;
+  }
+
+#ifdef SERIALON
+   Serial.println( Changed );
+#endif
+
+  // The Changed flag indicated that a button has been pressed 
+  if ( Changed == 1 )
+  {
+    // Send the buffer and reset the changed flag
+    Udp.beginPacket( Broadcast, 8888) ;
+    Udp.write( packetBuffer, 5 );
+    Udp.endPacket();
+    Changed = 0;
+    delay( 500 ); // A small delay to prevent UDP flooding while the button is pressed
+  }
+
+}
